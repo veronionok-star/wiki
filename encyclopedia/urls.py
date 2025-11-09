5ae31e117fd129ad18d74944baef69e98ba1164a
veronionok-star <veronionok@gmail.com>
2025-11-09 22:55:58 +0400

    added new page creation and page editing

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("wiki/", views.index, name="index"),
     path("wiki/search", views.search, name="search"),
     path("wiki/new", views.new, name="new"),
+    path("wiki/add", views.add, name="add"),
+    path("wiki/randomentry", views.random_entry, name="random_entry"),
+    path("wiki/save", views.save, name="save"),
+    path("wiki/edit/<str:name>", views.edit, name="edit"),
     path("wiki/<str:name>", views.entries, name="entries")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,10 +5,18 @@ from django.http import HttpResponseRedirect
 
 from . import util
 import markdown2
+import random
 
 class NewSearchForm(forms.Form):
     search_text = forms.CharField(max_length=100)
     search_text.label = ""
+
+class NewPageForm(forms.Form):
+    page_name = forms.CharField(max_length=100, label="Put the title of your page here:")
+    page_text = forms.CharField(
+        widget=forms.Textarea,
+        label="Put the text of your page here:"
+    )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -16,21 +24,6 @@ def index(request):
         "form": NewSearchForm,
         "text": "All pages"
     })
-
-def entries(request, name):
-    try:
-        entry_md = util.get_entry(name)
-        html = markdown2.markdown(entry_md)
-        return render(request, "encyclopedia/entry.html", {
-            "entry": html,
-            "entry_name": name,
-            "form": NewSearchForm,
-        })
-    except TypeError:
-        return render(request, "encyclopedia/error.html", {
-            "entry_name": name,
-            "form": NewSearchForm,
-        })
     
 def search(request):
     if request.method == "POST":
@@ -39,7 +32,6 @@ def search(request):
         form = NewSearchForm(request.POST)
         if form.is_valid():
             search_text = form.cleaned_data["search_text"]
-            print("form is valid")
             if search_text in all_entries:
                 return HttpResponseRedirect(f'/wiki/{search_text}')
             else:
@@ -56,15 +48,75 @@ def search(request):
             return render(request, "encyclopedia/index.html", {
                 "form": form
             })
+
 def new(request):
     return render(request, "encyclopedia/new.html", {
         "entries": util.list_entries(),
-        "form": NewSearchForm
+        "form": NewSearchForm,
+        "text_form": NewPageForm,
+        "title": 'Add new page'
     })
 
 def add(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            page_name = form.cleaned_data["page_name"]
+            page_text = form.cleaned_data["page_text"]
+            if page_name not in util.list_entries():
+                with open(f'entries/{page_name}.md', 'w') as f:
+                    f.write(page_text)
+                return HttpResponseRedirect(f'/wiki/{page_name}')
+            else:
+                return render(request, "encyclopedia/error.html", {
+            "form": NewSearchForm,
+            "error_title": "Page already exists",
+            "error_text": f'ERROR: A page with the title "{page_name}" already exists'
+            })
+
+def save(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            page_name = form.cleaned_data["page_name"]
+            page_text = form.cleaned_data["page_text"]
+            with open(f'entries/{page_name}.md', 'w') as f:
+                f.write(page_text)
+            return HttpResponseRedirect(f'/wiki/{page_name}')
+
+def random_entry(request):
+    entries = util.list_entries()
+    random_entry = random.choice(entries)
+    return HttpResponseRedirect(f'/wiki/{random_entry}')
+
+def edit(request, name):
+    editing = True
+    initial_data = {
+        "page_name": name,
+        "page_text": util.get_entry(name),
+    }
+    text_form = NewPageForm(initial=initial_data)
+    return render(request, "encyclopedia/new.html", {
+        "entry_name": name,
         "form": NewSearchForm,
-        "text": "All pages"
+        "text_form": text_form,
+        "title": f'Edit "{name}"',
+        "editing": editing
     })
+
+def entries(request, name):
+    try:
+        entry_md = util.get_entry(name)
+        html = markdown2.markdown(entry_md)
+        return render(request, "encyclopedia/entry.html", {
+            "entry": html,
+            "entry_name": name,
+            "form": NewSearchForm,
+        })
+    except TypeError:
+        return render(request, "encyclopedia/error.html", {
+            "entry_name": name,
+            "form": NewSearchForm,
+            "error_title": "Page not found",
+            "error_text": f'ERROR: A page with the title "{name}" does not exist'
+        })
